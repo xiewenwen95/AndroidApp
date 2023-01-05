c3df0e770874eb88c71ce00722c2f2aae8dbbc56
Celia-Hsu-114 <101543506+Celia-Hsu-114@users.noreply.github.com>
2023-01-05 17:44:55 +0800

    Update First Activity Using JSoup

--- app/src/main/java/iss/team6/thememorygame/ImageAdapter.java
+++ app/src/main/java/iss/team6/thememorygame/ImageAdapter.java
@@ -6,60 +6,55 @@ import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.squareup.picasso.Picasso;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class ImageAdapter extends RecyclerView.Adapter<ViewHolder> {
+ class ImageAdapter extends BaseAdapter{
+     private ArrayList<String> imageUrls;
+     private Context context;
 
-//    public ImageView[] imageViews = new ImageView[20];//create a list of ImageViews to be used for loading images in Picasso
-//
-//    private List<Drawable> images = new ArrayList<>();
-    private List<String> imageUrls;
-    private MainActivity mainActivity;
+     public ImageAdapter(ArrayList<String> imageUrls,Context context) {
+         this.imageUrls = imageUrls;
+         this.context = context;
+     }
+     @Override
+     public int getCount() {
+         return imageUrls.size();
+     }
 
-    public ImageAdapter(List<String> imageUrls, MainActivity mainActivity) {
-        this.imageUrls = imageUrls;
-        this.mainActivity = mainActivity;
-    }
+     @Override
+     public Object getItem(int position) {
+         return imageUrls.get(position);
+     }
 
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.image_layout, parent, false);
-        ViewHolder viewHolder = new ViewHolder(view);
-        return viewHolder;
-    }
+     @Override
+     public long getItemId(int position) {
+         return position;
+     }
 
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        //Picasso.get().load().resize(90,90).into(holder.imageView);
-        //holder.bind(imageViews[position]);
-        String imageUrl = imageUrls.get(position);
-        mainActivity.loadImages(imageUrl, holder.imageView);
-    }
+     @Override
+     public View getView(int position, View convertView, ViewGroup viewGroup) {
+         ImageView imageView;
+         if (convertView == null) {
+             imageView = new ImageView(context);
+             imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+         } else {
+             imageView = (ImageView) convertView;
+         }
 
-    @Override
-    public int getItemCount() {
-        return imageUrls.size();
-    }
-}
-class ViewHolder extends RecyclerView.ViewHolder {
-   ImageView imageView;
+         String url = imageUrls.get(position);
+         new ImageLoader().execute(imageView, url);
 
-    ViewHolder(View itemView) {
-        super(itemView);
-        imageView = itemView.findViewById(R.id.iv_picasso);
-
-    }
-    public void bind(ImageView imageView) {
-        this.imageView = imageView;
-    }
-}
+         return imageView;
+     }
+ }
 
--- app/src/main/java/iss/team6/thememorygame/MainActivity.java
+++ app/src/main/java/iss/team6/thememorygame/MainActivity.java
@@ -6,35 +6,46 @@ import androidx.recyclerview.widget.RecyclerView;
 
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
-import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Target;
+//import com.squareup.picasso.Picasso;
+//import com.squareup.picasso.Target;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private Button fetchBtn;
     private EditText fetchUrl;
+    private GridView gridView;
+    private ImageLoader imageLoader;
 
-    private RecyclerView recyclerView;
-
-
-    private ImageView[] imageViews = new ImageView[20];
-    private List<String>imgUrls=new ArrayList<>();
-    private int count = 0;
+    //private ImageView[] imageViews = new ImageView[20];
+    //private List<String>imgUrls=new ArrayList<>();
+    //private int count = 0;
 
     private SeekBar seekBar;
 
@@ -43,61 +54,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-       fetchUrl=(EditText)findViewById(R.id.fetchUrl);
-
-       fetchBtn=(Button) findViewById(R.id.fetchBtn);
-
-       recyclerView= findViewById(R.id.recyclerView);
-
-       seekBar=(SeekBar)findViewById(R.id.seekBar);
-
-        //  set the grid layout manager on the RecyclerView
-        //GridLayoutManager layoutManager = new GridLayoutManager(this, 4, RecyclerView.VERTICAL, false);
-
-        recyclerView.setLayoutManager(new GridLayoutManager(this,4));
-
-
-        recyclerView.setAdapter(new ImageAdapter(imgUrls,this));
-
-        recyclerView.setHasFixedSize(true);
+        fetchUrl=(EditText)findViewById(R.id.fetchUrl);
+        fetchBtn=(Button) findViewById(R.id.fetchBtn);
+        gridView=(GridView)findViewById(R.id.girdView);
+        fetchBtn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                fetch();
+            }
+         });
+        imageLoader = new ImageLoader();
     }
 
-    public void fetch(View v) {
-
-        fetchBtn.setOnClickListener(view -> {
-            for(int i=0;i<20;i++){
-            String url = fetchUrl.getText().toString()+i;
-                imgUrls.add(url);
-            try {
-                URL imageUrl = new URL(url);
-                loadImages(url,imageViews[count]);
-            } catch (MalformedURLException e) {
-                Toast.makeText(MainActivity.this, "Invalid URL", Toast.LENGTH_SHORT).show();
-            }
-            }
-        });
+    private void fetch() {
+        String imageUrl=fetchUrl.getText().toString();
+        new ImageUrlParser().execute(imageUrl);
     }
-    //use Picasso to download imgs,and attach the seekbar,which shows the progress of downloading processing
-    public  void loadImages(String url,ImageView imageView) {
-        for (int i = 0; i < 20; i++) {
-            Picasso.get()
-            .load(url).resize(90,90)//using  com.squareup.picasso.Callback interface to set two conditions as well as a seekbar to track downloading progress ,load the image into the list of imageViews
-            .into(new Target() {
-                @Override
-                public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-                    imageViews[count].setImageBitmap(bitmap);
-                }
-                @Override
-                public void onBitmapFailed(Exception e, Drawable errorDrawable) {
-                    Toast.makeText(MainActivity.this, "Unable to save images", Toast.LENGTH_SHORT).show();
-                }
-                @Override
-                public void onPrepareLoad(Drawable placeHolderDrawable) {
+private class ImageUrlParser extends AsyncTask<String,Void,ArrayList<String>> {
 
-                }
-            });
-            Picasso.get().load(url).into(imageView);
+    @Override
+    protected ArrayList<String> doInBackground(String... strings) {
+        String imgUrl=strings[0];
+        ArrayList<String>imageUrls=new ArrayList<>(20);
+        try{
+            Document doc=Jsoup.connect(imgUrl).get();
+            Elements imgElement=doc.select("img");
+            for(int i=0;i<20&& i < imgElement.size();i++){
+                imageUrls.add(imgElement.get(i).attr("src"));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return imageUrls;
     }
+    @Override
+    protected void onPostExecute(ArrayList<String> imageUrls) {
+        super.onPostExecute(imageUrls);
+        gridView.setAdapter(new ImageAdapter( imageUrls,MainActivity.this));
+    }
+}
+
 }
